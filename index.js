84fe0b211f0c98975925e499681c114066a6a01f
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-11-15 15:40:24 +0100

    greetings script

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello boolean");
+function greetings(params) {
+  let greeting = "Ciao ";
+  greeting += params;
+  console.log(greeting);
+}
+
+const params = process.argv.slice(2).join(" ");
+
+greetings(params);
